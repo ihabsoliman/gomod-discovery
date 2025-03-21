f6f02cfca9e6e1e358c361f53fcfc17c36fc6509
Ihab Soliman <2053386+ihabsoliman@users.noreply.github.com>
2025-03-21 19:22:11 -0400

    remove debug code
    Signed-off-by: Ihab Soliman <2053386+ihabsoliman@users.noreply.github.com>

--- src/log.py
+++ src/log.py
@@ -1,12 +1,8 @@
 import logging
 import sys
-import os
 
 logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler(sys.stdout)])
 logger = logging.getLogger("main")
-
-for key, value in os.environ.items():
-    print(f"{key}: {value}")
 
 
 def get_logger(name: str, level=logging.INFO) -> logging.Logger:
