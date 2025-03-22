ae49a71cefca3f8681b9ea1a6fb2616c562dec89
Ihab Soliman <2053386+ihabsoliman@users.noreply.github.com>
2025-03-21 20:11:55 -0400

    handle go module discovery
    Signed-off-by: Ihab Soliman <2053386+ihabsoliman@users.noreply.github.com>

--- src/entry.py
+++ src/entry.py
@@ -1,11 +1,22 @@
-from js import Response, Object
+from js import Response, Request, Headers, Object
 from pyodide.ffi import to_js as _to_js
+from pyodide.ffi import JsProxy
 
 from urllib.parse import urlparse, urlunparse, parse_qs, ParseResult
 from typing import Optional
+from dataclasses import dataclass
 
 from router import Router
 from log import get_logger
+
+
+@dataclass
+class RouteRequest:
+    Url: ParseResult
+    QueryParams: dict
+    Method: str
+    Request: Request
+    Env: JsProxy
 
 
 # Configure logging
@@ -20,41 +31,72 @@ async def on_fetch(request, env):
     if not required_env_variables(env):
         return Response.new("", status=500)
 
-    url, params = parse_url(request.url)
+    url, qs = parse_url(request.url)
     if url is None:
         return Response.new("Invalid URL", status=400)
     request_method = request.method.upper()
+    route_request = RouteRequest(
+        Url=url, QueryParams=qs or {}, Method=request_method, Request=request, Env=env
+    )
     logger.debug(f"Request method: {request_method}")
-    logger.debug(f"Request URL: {strip_query_params(url)} with params: {params}")
+    logger.debug(f"Request URL: {strip_query_params(url)} with qs: {qs}")
     handler, params = router.match(request.url, request_method)
+
     if handler:
         logger.debug(
             f"Handler found for URL: {strip_query_params(url)} with params: {params}"
         )
-        response = handler(**params)
-        logger.debug(f"Response: {response}")
-        return Response.json(to_js(response))
+        return handler(route_request, **params)
     else:
         logger.debug(f"No handler found for URL: {strip_query_params(url)}")
         return Response.new("Not found", status=404)
 
 
 @router.route("/")
-def get_root():
-    logger.debug("Handling get_root")
-    return {"action": "get_root"}
+def handle_root(route_request: RouteRequest):
+    logger.debug("Handling handle_root")
+    return {"action": "handle_root"}
 
 
-@router.route("/{module}/@latest")
-def get_latest_module(module: str):
-    logger.debug(f"Handling get_latest_module for module: {module}")
-    return {"action": "get_latest", "module": module}
+@router.route("/{site}/{owner}/{project}")
+def handle_module(route_request: RouteRequest, site: str, owner: str, project: str):
+    logger.debug("Handling handle_module")
+    if ("go-get" not in route_request.QueryParams) or (
+        route_request.QueryParams["go-get"][0] != "1"
+    ):
+        return Response.json(
+            to_js(
+                {
+                    "action": "handle_module",
+                    "site": site,
+                    "owner": owner,
+                    "project": project,
+                }
+            ),
+            status=404,
+        )
 
+    domain = route_request.Url.netloc
+    github_account = route_request.Env.GITHUB_ACCOUNT
 
-@router.route("/{module}/@v/latest")
-def get_latest_module_alt(module: str):
-    logger.debug(f"Handling get_latest_module_alt for module: {module}")
-    return {"action": "get_latest_alt", "module": module}
+    headers = Headers.new({"content-type": "text/html; charset=utf-8"}.items())
+    return Response.new(
+        f"""<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<meta name="go-import" content="{domain}/{project} git https://github.com/{github_account}/{project}">
+<meta name="go-source" content="{domain}/{project} https://github.com/{github_account}/{project} https://github.com/{github_account}/{project}/tree/master{{/dir}} https://github.com/{github_account}/{project}/blob/master{{/dir}}/{{file}}#L{{line}} ">
+<title>{project}</title>
+</head>
+
+<body>
+<a href=\"https://{domain}\">https://{domain}</a>
+</body>
+</html>
+        """,
+        headers=headers,
+    )
 
 
 #########
